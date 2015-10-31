3fe51d0cff0f7fb029b481c839c2568bbb212f54
Michael Koppelman <michael@clockwork.net>
2015-10-31 11:34:58 -0500

    Modified to calculate volume from the sensor for a specific test

--- ultrasonic/ultra.ino
+++ ultrasonic/ultra.ino
@@ -8,32 +8,38 @@ const int greenLED = D4;
 
 // the LED that we are currently flashing
 int flashLED = redLED;
-int delay_time=50;
+int delay_time=60;
 
 // t-nought of our execution timer
 unsigned long t0;
 
 // Minimum time to wait between updates
-const unsigned int execution_delay = 500;
+const unsigned int execution_delay = 100;
 
 // converts microseconds into cm
 const float us2s = 0.01715;
 
 // The last distance measurement,
 // which is registered as a Particle variable on the Cloud
-double  distance = 0.0;
+double  distance = 0.0,
+        volume = 0.0,
+// x-nought is our reference for zero
+        x0=25.3134,
+        slope=-85.8571,
+        intercept=2126.47;
 
 void setup() {
   t0 = millis();
   Particle.variable("distance", distance);
+  Particle.variable("volume", volume);
 
   pinMode(redLED, OUTPUT);
   pinMode(yellowLED, OUTPUT);
   pinMode(greenLED, OUTPUT);
 
-  Serial.begin( 9600 ); //ignored, I'm told
+  /*Serial.begin( 9600 ); //ignored, I'm told
   Serial.println( "ultra.ino : Printing to USB via serial." );
-  Serial.printlnf( "%s\t%s", "t", "x" );
+  Serial.printlnf( "%s\t%s\t%s", "t", "x", "V" );*/
 }
 
 void pulseOut(int s) {
@@ -52,17 +58,18 @@ void loop() {
   //This is our execution timer. We get a new reading every
   if( millis() - t0 > execution_delay) {
     distance = read_distance(sensor);
-    if( distance > 30.0) {
+    volume = (distance*slope)+intercept;
+    if( volume < 1000) {
       flashLED = greenLED;
     }
-    else if( distance > 15.0) {
+    else if( volume < 1500) {
       flashLED = yellowLED;
     }
     else {
       flashLED = redLED;
     }
-    delay_time = distance;
-    Serial.printlnf( "%i\t%f", millis(), distance );
+    delay_time = 1.0/distance*1000.0;
+    /*Serial.printlnf( "%i\t%f\t%f", millis(), distance, volume );*/
     t0=millis();
   }
   digitalWrite(flashLED, HIGH);
